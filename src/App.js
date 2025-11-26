d25280f17827024c1626ac240411af625c749ef3
MrManoj124 <manojmanorooban@gmail.com>
2025-11-26 22:46:43 +0530

    Update the Api.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './Context/AuthContext';
 import LoginForm from './Components/JsFiles/LoginForm';
 import RegisterForm from './Components/JsFiles/RegiterForm';
 //import Dashboard from './Components/JsFiles/Dashboard';
 
 function App() {
   return (
+    <Router>
+      <AuthProvider>
     <div className="App">
-      <LoginForm/>
-      <RegisterForm/>
+      <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginForm/>} />
+        <Route path="/register" element={<RegisterForm/>} />
+      </Routes>
     </div>
+    </AuthProvider>
+    </Router>
   );
 }
 
--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,6 +1,6 @@
 
 
-const API_URL=PROCESS_APP_API_URL || 'http://localhost:5000/api';
+const API_URL=process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 const apicall = async (endpoint, options ={})=>{
     try{
         const response = await fetch(`${API_URL}${endpoint}`,{
